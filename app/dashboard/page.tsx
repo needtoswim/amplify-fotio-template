6f75463a320ffb8b4f3b06d685e88ed5320a5df2
john <lyonsalt@gmail.com>
2024-09-25 16:30:00 +1000

    Leaving fotio-3

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+
+    return
+        (
+            <div className="text-xl text-red">
+    
+                <p>Dashboard Page</p>;
+            </div>
+    
+        )
+  }
