f8f6d8c95f86d2af11f9dc4b6109ed283ad209ad
john <lyonsalt@gmail.com>
2024-10-08 06:56:12 +1100

    fotio-6

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -2,39 +2,103 @@ import Link from 'next/link';
 
 const Footer = () => {
     return (
-      <footer className='z-10 py-10 text-white p-2 '>
+      <footer className='z-10 py-10 text-white p-2 bg-cyan-950'>
         {/* <div className='container'>
           <p className='mt-4 text-xl text-white'>
             &copy; {new Date().getFullYear()} fotio.com.  Branch Authorisation 20th July 24
           </p>
           
-        </div> */}
-        <section>
-           <p className="text-xl text-white">
+        </div>  */}
+        <section className='background: divide-sky-950'
+          >
+           <p className="text-xl text-white ">
             &copy; {new Date().getFullYear()} fotio.com.
           </p>
-
-          <div className="flex flex-row p-6 gap-x-10 ">
-            <div className="flex box-border h-50 w-48 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
-              <p className="text-wrap text-lg">
-                Fotio is a platform to securely store images within galleries.
-                See documentation for recommendatons on galleries and image types.
+          <div className="hidden md:flex flex-row py-2 gap-x-10">
+            <div className="flex w-48 px-10 box-border h-50 p-1 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-left text-white">
+                Galleries
               </p> 
             </div> 
 
-            <div className="box-border h-50 w-64 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
-              <p className="text-wrap text-lg">
-                Easily categorise and tag content to suit your preferences. Think of categories as the branches and leaves of a tree of folders. You can change this structure any time.
+            <div className="flex w-48 px-10 box-border h-50 p-2 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+                Structure
               </p>
             </div>
-            <div className="box-border h-50 w-64 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
-              <p className="text-wrap text-lg">
-              Access is by invitation of the member/curator. So you share galleries with family, friends and others at your discretion. You can specify different levels of access for each guest.
+            <div className="flex w-48 px-10 box-border h-50 p-2 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+                Sharing
               </p>
             </div>  
-            <div className="box-border h-50 w-64 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
+            <div className="flex w-48 px-10 box-border h-50 p-2 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+               Security 
+              </p>
+            </div> 
+            <div className="flex w-48 px-10 box-border h-50 p-2 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+               Pricing 
+              </p>
+            </div> 
+          </div>  
+          <div className="hidden md:flex flex-row py-2 gap-x-10">
+            <div className="box-border h-50 w-48 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
               <p className="text-wrap text-lg">
-               All images are encrypted as they are saved. Encrypting data means encoding it in such a way that it can only be restored to its original form if the person or system decoding it has the appropriate key. 
+                A platform to securely store images within galleries.
+              
+              </p> 
+            </div> 
+
+            <div className="box-border h-50 w-48 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
+              <p className="text-wrap text-lg">
+                Dynamically categorise and tag content to suit your preferences. 
+              </p>
+            </div>
+            <div className="box-border h-50 w-48 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
+              <p className="text-wrap text-lg">
+              Share galleries with family, friends and others at your discretion. 
+              </p>
+            </div>  
+            <div className="box-border h-50 w-48 p-2 border-2 border-slate-50 border-spacing-0 basis:1/4">
+              <p className="text-wrap text-lg">
+               All images are encrypted as they are saved. 
+              </p>
+            </div> 
+            <div className="box-border h-50 w-48 p-2 border-2 border-slate-50 border-spacing-2 basis:1/4">
+              <p className="text-wrap text-lg">
+               Guests free. Members charged according to usage. 
+              </p>
+            </div>
+          </div>  
+          <div className="hidden md:flex flex-row py-2 gap-x-10">
+            <div className="flex w-48 px-10 box-border h-50 p-1 border-1 border-slate-50 basis:1/4">
+            <Link
+              href='#galleries'
+              className='text-xl text-white mr-4 hover:text-white hover:text-2xl rounded' 
+            >
+              more...
+          </Link> 
+            </div> 
+
+            <div className="flex w-48 px-10 box-border h-50 p-2 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+                Structure
+              </p>
+            </div>
+            <div className="flex w-48 px-10 box-border h-50 p-2 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+                Sharing
+              </p>
+            </div>  
+            <div className="flex w-48 px-10 box-border h-50 p-2 border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+               Security 
+              </p>
+            </div> 
+            <div className="flex w-48 px-10 box-border h-50 p- border-1 border-slate-50 basis:1/4">
+              <p className="text-2xl text-center text-white">
+               Pricing 
               </p>
             </div> 
           </div>  
